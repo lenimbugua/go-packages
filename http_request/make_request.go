0f635407470e95dcc43c9918eb33c5d3c47f9154
Leonard Mbugua <lenimbugua@gmail.co>
2023-12-07 18:05:58 +0300

    validate headers

--- http_request/make_request.go
+++ http_request/make_request.go
@@ -1,8 +1,12 @@
 package http_request
 
 import (
+	"context"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"time"
 )
 
 type requestHeaders map[string]string
@@ -11,42 +15,67 @@ type requestHeaders map[string]string
   MakeHTTPRequest creates a new request,
   Sets request headers if any,
   Sends the request, and
-  Finaly returns the response
+  Finaly returns the response.
 */
 
-func MakeHTTPRequest(client *http.Client, httpMethod string, url string, body io.Reader, headers requestHeaders) ([]byte, error) {
-	//Create a new request.
+func MakeHTTPRequest(ctx context.Context, client *http.Client, httpMethod string, url string, body io.Reader, headers requestHeaders, timeoutDuration time.Duration) ([]byte, error) {
+	// Validate HTTP method
+	if !isValidHTTPMethod(httpMethod) {
+		return nil, fmt.Errorf("invalid HTTP method: %s", httpMethod)
+	}
+
+	// Create a new request.
 	request, err := http.NewRequest(httpMethod, url, body)
 	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %v", err)
+	}
+
+	// Validate and set request headers
+	if err := validateAndSetHeaders(request, headers); err != nil {
 		return nil, err
 	}
 
-	//Set request headers
-	for key, value := range headers {
-		request.Header.Set(key, value)
-	}
+	// Use context for the request
+	request = request.WithContext(ctx)
 
-	//Make request
+	// Make request with timeout
+	client.Timeout = timeoutDuration 
 	response, err := client.Do(request)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to make request: %v", err)
 	}
-
-	// Close response body
-	defer func() error {
+	defer func() {
 		if response.Body != nil {
 			err := response.Body.Close()
 			if err != nil {
-				return err
+				log.Printf("failed to close response body: %v", err)
 			}
 		}
-		return nil
 	}()
 
 	// Read the response body
 	res, err := io.ReadAll(response.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to read response body: %v", err)
 	}
+
 	return res, nil
+}
+
+func isValidHTTPMethod(method string) bool {
+	switch method {
+	case http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete:
+		return true
+	}
+	return false
+}
+
+func validateAndSetHeaders(request *http.Request, headers requestHeaders) error {
+	for key, value := range headers {
+		if key == "" || value == "" {
+			return fmt.Errorf("invalid header: %s=%s", key, value)
+		}
+		request.Header.Set(key, value)
+	}
+	return nil
 }
